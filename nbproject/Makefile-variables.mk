6d5816861bc266cde44e683bd5df0cfb737c3960
Rachana <rachana.h67@gmail.com>
2026-08-30 10:16:07 +0530

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Git_practice.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Git_practice.X.production.hex
---
+++ newmain.c
@@ -0,0 +1,11 @@
+#include <xc.h>
+
+void main(void)
+{
+    TRISCbits.TRISC0 = 0;   // RC0 as output
+
+    while(1)
+    {
+        LATCbits.LATC0 = 1; // LED ON
+    }
+}
